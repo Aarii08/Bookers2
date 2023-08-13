f052d58c34b132df3e1b1650b3f045e00c3b5806
Aarii08 <140925809+Aarii08@users.noreply.github.com>
2023-08-13 07:37:51 +0000

    ファイルの設定までOK

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root to: "homes#top"
+  get "home/about" => "homes#about", as: "about"
+
+  devise_for :users
+
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
+
+
 
 
 
